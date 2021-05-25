d26da88e02511fd8a39bdac6f755e1a4f68ec1c1
Emanuel Campos Jasso <isw.ecamposj@gmail.com>
2021-05-24 21:55:11 -0500

    inicio de proyecto de pokedex

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {StackNavigator} from './src/navigation/MainNavigation';
+
+const App = () => {
+  return <StackNavigator />;
+};
+
+export default App;
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const HomeScreen = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 52}}>HomeScreen</Text>
+    </View>
+  );
+};
